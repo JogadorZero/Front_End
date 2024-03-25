ac685ecea82ca42487c747582ab4f839d356d227
JogadorZero <jz.jogadorzero@gmail.com>
2024-03-25 20:23:16 -0300

    Create script.js

---
+++ Javascript/script.js
@@ -0,0 +1,47 @@
+// console.log(typeof 5)
+// console.log(typeof "Hello world")
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 / 2)
+// console.log(2 % 2)
+// console.log(`A soma de 2 + 2 = ${2 + 2}`)
+// console.log(2 == 2)
+// console.log(2 === 2)
+// console.log(3 != 2)
+// console.log(5 >= 5)
+// console.log(3 != 3)
+// console.log(3 !== 3)
+// console.log(3 !== '3')
+// console.log(3 > 2 || 2 < 1);
+// console.log(3 > 2 && 2 < 1);
+// console.log(true);
+// console.log(!true)
+// console.log(false)
+// console.log(!false)
+
+/*
+const user = 'Lucas';
+if (user === 'Lucas'){
+    console.log('Usuário autenticado!');
+}
+else {
+    console.log('Usuário inexistente');
+}
+*/
+
+// const user = 'Lucas';
+// user === 'Lucas' ? console.log('Usuário autenticado!') : console.log('Usuário inexistente');
+
+const curso = 'ENC';
+switch(curso){
+    case 'ENC':
+        console.log('Engenharia da Computação');
+        break;
+    case 'ESW':
+        console.log('Engenharia de Software');
+        break;
+    case 'EMC':
+        console.log('Engenharia de Mecatrônica');
+        break;
+}
